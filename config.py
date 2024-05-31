5487e2578341a6d56c412135612cbb2af44f2565
ritanlisa <jack1122334455@126.com>
2024-05-31 17:43:48 +0800

    update configure files

--- config.py
+++ config.py
@@ -1,30 +1,44 @@
 # coding:UTF-8
 
 
-import os
-
-
 DEBUG = True
 
 WTF_CSRF_ENABLED = False
 
 SECRET_KEY = '!@#$%8F6F98EC3684AECA1DC44E1CB816E4A5^&*()'
 
-# if current os is linux
-if os.name == 'posix':
-    UPLOAD_FOLDER = '/home/IPC/PCAP/'
-    FILE_FOLDER = '/home/IPC/Files/'
-    PDF_FOLDER = '/home/IPC/Files/PDF/'
-    WORKFLOW_FOLDER = '/home/IPC/Files/Workflow/'
-    NETWORK_FOLDER = '/home/IPC/Files/Network/'
-    CICFLOWMETER_PATH = '/home/IPC/CICFlowMeter-4.0/bin/cfm'
-    CSV_FOLDER = '/home/IPC/Files/CSV/'
-elif os.name == 'nt':
-    UPLOAD_FOLDER = './PCAP/'
-    FILE_FOLDER = './Files/'
-    PDF_FOLDER = './PDF/'
-    WORKFLOW_FOLDER = './Workflow/'
-    NETWORK_FOLDER = './network/'
-    LOG_FOLDER = './log/'
-    CICFLOWMETER_PATH = './CICFlowMeter-4.0/bin/cfm'
-    CSV_FOLDER = './CSV/'
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+UPLOAD_FOLDER = 'tmp/PCAP/'
+FILE_FOLDER = 'tmp/Files/'
+PDF_FOLDER = 'tmp/Files/PDF/'
+CSV_FOLDER = 'tmp/Files/csv/'
+NETWORK_FOLDER = 'network/'
+CICFLOWMETER_PATH = 'CICFlowMeter-4.0/CICFlowMeter-4.0/bin'
+
+UPLOAD_FOLDER = os.path.join(basedir, UPLOAD_FOLDER)
+FILE_FOLDER = os.path.join(basedir, FILE_FOLDER)
+PDF_FOLDER = os.path.join(basedir, PDF_FOLDER)
+CSV_FOLDER = os.path.join(basedir, CSV_FOLDER)
+NETWORK_FOLDER = os.path.join(basedir, NETWORK_FOLDER)
+CICFLOWMETER_PATH = os.path.join(basedir, CICFLOWMETER_PATH)
+
+# if using windows fs
+if os.name == 'nt':
+    UPLOAD_FOLDER = UPLOAD_FOLDER.replace('/', '\\')
+    FILE_FOLDER = FILE_FOLDER.replace('/', '\\')
+    PDF_FOLDER = PDF_FOLDER.replace('/', '\\')
+    CSV_FOLDER = CSV_FOLDER.replace('/', '\\')
+    NETWORK_FOLDER = NETWORK_FOLDER.replace('/', '\\')
+    CICFLOWMETER_PATH = CICFLOWMETER_PATH.replace('/', '\\')
+elif os.name == 'posix':
+    UPLOAD_FOLDER = UPLOAD_FOLDER.replace('\\', '/')
+    FILE_FOLDER = FILE_FOLDER.replace('\\', '/')
+    PDF_FOLDER = PDF_FOLDER.replace('\\', '/')
+    CSV_FOLDER = CSV_FOLDER.replace('\\', '/')
+    NETWORK_FOLDER = NETWORK_FOLDER.replace('\\', '/')
+    CICFLOWMETER_PATH = CICFLOWMETER_PATH.replace('\\', '/')
+else:
+    raise OSError('Unsupported operating system')
